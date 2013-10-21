29124f4e4e64c6f5a9388f70bfa1b4fc457415af
duckens <duckens@duckens-ThinkPad-T420.(none)>
2013-10-22 00:32:14 +0400

    images and video ht2 didnt make it. Only a half((

--- image_video/ht02/andrew.borozdin.task-02.cpp
+++ image_video/ht02/andrew.borozdin.task-02.cpp
@@ -68,27 +68,55 @@ Mat readImage(std::string path){
 	return imread(path.c_str(), CV_LOAD_IMAGE_COLOR);
 }
 
-Mat concatImages(std::vector<Mat> images){
+Mat concatImages(std::vector<Mat> images, int limitInRow = 0){
 
 	int size = images.size();
 	if(size == 0){
 		return Mat();
 	}
+	if( (limitInRow == 0) || (limitInRow > size) ){
+		limitInRow = size;
+	}
 
+	int big_cols = std::min(size, limitInRow);
+	int big_rows = 1 + ( size + limitInRow - 1 ) % limitInRow;
+
+	int cols_thisRowOfImages = 0;
+	int rows_thisRowOfImages = 0;
 	int cols = 0;
 	int rows = 0;
+
+
 	for(int i = 0; i < size; i++){
-		cols += images[i].cols;
-		rows = std::max(rows, images[i].rows);
+
+		cols_thisRowOfImages += images[i].cols;
+		rows_thisRowOfImages = std::max(rows_thisRowOfImages, images[i].rows);
+
+		if( ( (i + 1) % limitInRow == 0 ) || (i == size - 1) ){
+			cols = std::max(cols, cols_thisRowOfImages);
+			cols_thisRowOfImages = 0;
+			rows += rows_thisRowOfImages;
+		}
 	}
+	
 
 	Mat result_image(rows, cols, images[0].type());
 
 	int currentPosition_cols = 0;
-	for(int i = 0; i < size; i++){
-		cv::Rect regionOfInterest( cv::Point( currentPosition_cols, 0 ), cv::Size( images[i].cols, images[i].rows ));
-		images[i].copyTo( result_image( regionOfInterest ) );
-		currentPosition_cols += images[i].cols;
+	int currentPosition_rows = 0;
+	for(int i = 0; i < big_rows; i++){
+
+		int nextShiftRows = 0;
+
+		for(int j = 0; (j < big_cols) && (j + i * big_cols < size); j++){
+			cv::Rect regionOfInterest( cv::Point( currentPosition_cols, currentPosition_rows ), cv::Size( images[i].cols, images[i].rows ));
+			images[j + i * big_cols].copyTo( result_image( regionOfInterest ) );
+			currentPosition_cols += images[i].cols;
+			nextShiftRows = std::max(nextShiftRows, images[i].rows);
+		}
+
+		currentPosition_rows += nextShiftRows;
+		currentPosition_cols = 0;
 	}
 	
 	return result_image;
@@ -103,7 +131,6 @@ bool addSaltPepper(Mat input_image, Mat& output_image, double p, double q){
 	int rows = input_image.rows;
 	int cols = input_image.cols;
 	output_image = Mat(rows, cols, input_image.type());
-	// input_image.copyTo(output_image);
 
 	const int largeNumber = 1000000;
 	const int borderSalt = largeNumber * p;
@@ -152,6 +179,8 @@ int main(int argc, char* argv[]){
 		return -1;
 	}
 	
+
+		
 	std::pair< std::string, std::string > namesPair = parseFilename(argv[1]);
 	std::string imageFileName = namesPair.first;
 	std::string imageFileExtension = namesPair.second;
@@ -163,7 +192,7 @@ int main(int argc, char* argv[]){
 
 
 
-	
+	// task 1.1
 	std::vector< std::pair< double, double > > gradations;
 	gradations.push_back(std::pair< double, double >( 0.5, 0.0 ));
 	gradations.push_back(std::pair< double, double >( 1.0, -125.0 ));
@@ -180,17 +209,16 @@ int main(int argc, char* argv[]){
 
 
 
-
+	// task 1.2
 	std::vector<Mat> images_normalizeEqualizeHist(4);
 	cvtColor(image, images_normalizeEqualizeHist[0], CV_BGR2GRAY);
-	// showImage(images_normalizeEqualizeHist[2], "norm");
 	equalizeHist(images_normalizeEqualizeHist[0], images_normalizeEqualizeHist[1]);
 	normalize(images_normalizeEqualizeHist[0], images_normalizeEqualizeHist[2]);
 	images_normalizeEqualizeHist[3] = images_normalizeEqualizeHist[1] - images_normalizeEqualizeHist[2];
 	showImage(concatImages(images_normalizeEqualizeHist), imageFileName + "_normalizeEqualizeHist");
 
 
-
+	// task 1.3
 	std::vector< std::pair< double, double > > saltPepperProbabilities;
 	saltPepperProbabilities.push_back(std::pair< double, double >( 0.05, 0.05 ));
 	saltPepperProbabilities.push_back(std::pair< double, double >( 0.1, 0.1 ));
@@ -206,6 +234,7 @@ int main(int argc, char* argv[]){
 
 
 
+	// task 1.4
 	std::vector< std::pair< double, double > > gaussianNoiseParams;
 	gaussianNoiseParams.push_back(std::pair< double, double >( 0.0, 30.0 ));
 	gaussianNoiseParams.push_back(std::pair< double, double >( 0.0, 60.0 ));
@@ -219,7 +248,74 @@ int main(int argc, char* argv[]){
 	std::cout << "Gaussian Noise Ok=" << allOk << std::endl;
 	showImage(concatImages(images_gaussianNoise), imageFileName + "_gaussianNoise");
 
+
+
+
+
+	// task 1.5
+	std::vector<int> sizes(3);
+	sizes[0] = 3;
+	sizes[1] = 5;
+	sizes[2] = 7;
+	for(int i = 1; i <= 3; i++){
+		std::vector<Mat> images_blur;
+		Mat this_image = images_saltPepper[i];
+		for(int j = 0; j < 3; j++){// task 1.5
+
+			Mat blurred_image;
+			blurred_image = Mat(this_image.rows, this_image.cols, this_image.type());
+			int sz = sizes[j];
+			blur(this_image, blurred_image, cv::Size(sz, sz));
+			images_blur.push_back(blurred_image);
+			
+		}
+		for(int j = 0; j < 3; j++){
+
+			Mat blurred_image;
+			blurred_image = Mat(this_image.rows, this_image.cols, this_image.type());
+			int sz = sizes[j];
+			GaussianBlur(this_image, blurred_image, cv::Size(sz, sz), 10);
+			images_blur.push_back(blurred_image);
+			
+		}
+		for(int j = 0; j < 3; j++){
+
+			Mat blurred_image;
+			blurred_image = Mat(this_image.rows, this_image.cols, this_image.type());
+			int sz = sizes[j];
+			medianBlur(this_image, blurred_image, sz);
+			images_blur.push_back(blurred_image);
+			
+		}
+		showImage(concatImages(images_blur, 3), imageFileName + "_" + intToString(i) + "_blur");
+	}
 	
+
+
+	// Дальше, честно говоря просто не успел. С ДПФ я знаком по работе, но рзобраться с ним для изображений было бы определенно интересно.
+	// Опять же, как я уже говорил - отчетность мне не важна в данном случае.
+	// Однако надеюсь что доделаю ДПФ) - это просто интересно)
+
+
+	/*
+	Mat image_gray;
+	cvtColor(image, image_gray, CV_BGR2GRAY);
+	Mat padded;
+	int m = getOptimalDFTSize( image_gray.rows );
+	int n = getOptimalDFTSize( image_gray.cols ); // on the border add zero values
+	copyMakeBorder(image_gray, padded, 0, m - image_gray.rows, 0, n - image_gray.cols, BORDER_CONSTANT, Scalar::all(0));
+
+	Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+	Mat complexI;
+	merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
+
+	dft(complexI, complexI);
+	*/
+
+	// std::vector<Mat> image_dft(10);
+	// dft(image_dft[0], image_dft[1]);
+	// showImage(concatImages(image_dft), imageFileName + "_dft");
+
 
 
 	waitKey(0);
